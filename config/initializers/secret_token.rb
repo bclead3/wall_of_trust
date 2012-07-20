0eca0c1be09c712eb785e150527f6f8457934256
bclead3 <bclead3@yahoo.com>
2012-07-20 18:34:38 -0500

    initial setup for Wall of Trust

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WallOfTrust::Application.config.secret_token = 'e34179e20ac2161ba3452166edbe8cd6fa8589f103d75091ed0c499f1043e2146d17291c389d97a2c8446b4ea322f411b5bde18f671afc867574a3979e6b4544'
